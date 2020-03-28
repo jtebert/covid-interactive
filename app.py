a1ef460a0bbc3b89b4e5e303a903f841ca7af44e
Julia Ebert <julia@juliaebert.com>
2020-03-28 13:24:06 -0400

    There's a map now

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
 import datetime
+from urllib.request import urlopen
+import json
+
+import plotly.express as px
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-
 from dash.dependencies import Input, Output
 
 import pandas as pd
@@ -18,13 +21,18 @@ data_dir = 'covid-19-data'
 county_data_filename = 'us-counties.csv'
 state_data_filename = 'us-states.csv'
 
-county_df = dp.import_data(os.path.join(data_dir, county_data_filename))
-state_df = dp.import_data(os.path.join(data_dir, state_data_filename))
-state_df = dp.get_doubling_rate(state_df, 'cases')
-state_df = dp.get_doubling_rate(state_df, 'deaths')
+county_df = dp.process_data(dp.import_data(os.path.join(data_dir, county_data_filename)))
+state_df = dp.process_data(dp.import_data(os.path.join(data_dir, state_data_filename)))
+# state_df = dp.get_doubling_rate(state_df, 'cases', 'deaths')
+# county_df = dp.get_doubling_rate(county_df, 'cases', 'deaths')
+county_df_nanless = county_df[county_df['fips'].notnull()]
 
 states = state_df.state.unique()
 states.sort()
+
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+
 
 # ------------------------------------------------------------------------------
 
@@ -43,12 +51,13 @@ cases_or_deaths_options = {
     'deaths': 'Deaths'
 }
 y_data_options = {
-    '': 'Total count',
-    'change': 'Daily change',
-    'doubling_rate': 'Doubling rate (days)'
+    '': 'Total Count',
+    'change': 'Daily Change (ratio)',
+    'doubling_rate': 'Doubling Rate (days)'
 }
 
 # ------------------------------------------------------------------------------
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -79,12 +88,83 @@ app.layout = html.Div(children=[
         value=''
     ),
 
+    html.Label('Map date:'),
+    dcc.DatePickerSingle(
+        id='date-picker',
+        min_date_allowed=state_df['date'].min(),
+        max_date_allowed=state_df['date'].max(),
+        # initial_visible_month=dt(2017, 8, 5),
+        date=str(state_df['date'].max()),
+        display_format='MMM D YYYY'
+    ),
+
+    dcc.Graph(
+        id='case-map',
+        style={'height': 900}
+    ),
+
     dcc.Graph(
         id='case-count',
         style={'height': 900}
     ),
 
 ])
+
+
+def get_column_name(cases_or_deaths, y_data):
+    # Get the data pandas column name based on selected options
+    y_key = cases_or_deaths
+    if y_data:
+        y_key = y_key + '_' + y_data
+    return y_key
+
+
+@app.callback(
+    Output(component_id='case-map', component_property='figure'),
+    [Input(component_id='yaxis-type', component_property='value'),
+     Input(component_id='cases-or-deaths', component_property='value'),
+     Input(component_id='y-data', component_property='value'),
+     Input(component_id='date-picker', component_property='date'),
+     ]
+)
+def update_case_map(yaxis_type, cases_or_deaths, y_data, use_date):
+    y_key = get_column_name(cases_or_deaths, y_data)
+
+    title_str = '{} of {}'.format(
+        y_data_options[y_data], cases_or_deaths_options[cases_or_deaths])
+
+    fig = px.choropleth(
+        dp.col_filter(county_df_nanless, date=use_date),
+        locations='fips',
+        color=y_key,
+        geojson=counties,
+        color_continuous_scale="Viridis",
+        # range_color=(0, 100),
+        scope="usa",
+        hover_name='title',
+        # locationmode='USA-states',
+        # text=county_df_nanless['cases'],
+        labels={cases_or_deaths_options[cases_or_deaths]: cases_or_deaths},
+        # county_outline={'color': 'rgb(255,255,255)', 'width': 0.5}
+    )
+
+    fig.update_layout(
+        title=title_str + ' by County on ' + str(use_date).split(' ')[0],
+        coloraxis_colorbar=dict(
+            title=title_str,
+        ),
+        # county_df_nanless['cases']
+    )
+    # fig.update_traces(marker=dict(line=dict(color="green")))
+    # fig.update_traces(marker_line_color='black')
+    line_color = 'white'
+    fig.update_traces(
+        marker_line={'color': line_color, 'width': 0.5},
+        text='cases',
+        # colorbar_title_text="This is a test"
+    )
+    fig.update_geos(showsubunits=True, subunitcolor=line_color, subunitwidth=1.5)
+    return fig
 
 
 @app.callback(
@@ -97,18 +177,17 @@ def update_case_count(yaxis_type, cases_or_deaths, y_data):
     title_str = '{} of {}'.format(
         y_data_options[y_data], cases_or_deaths_options[cases_or_deaths])
 
-    # Get the data pandas column name based on selected options
-    y_key = cases_or_deaths
-    if y_data:
-        y_key = y_key + '_' + y_data
+    y_key = get_column_name(cases_or_deaths, y_data)
 
     # Set bounds for doubling rate
+    lower_bound = 0
     if y_data == 'doubling_rate':
         upper_bound = 30
     else:
         upper_bound = np.max(state_df[y_key])
     if yaxis_type == 'log':
         upper_bound = np.log10(upper_bound)
+        lower_bound = 0
 
     date_range = [
         np.min(state_df['date']),
@@ -133,7 +212,7 @@ def update_case_count(yaxis_type, cases_or_deaths, y_data):
             'yaxis': {
                 'type': yaxis_type,
                 'title': title_str,
-                'range': [0, upper_bound],
+                'range': [lower_bound, upper_bound],
             },
             'xaxis': {
                 'range': date_range,
--- data_process.py
+++ data_process.py
@@ -7,7 +7,9 @@ import numpy as np
 
 def import_data(filename):
     def dateparser(x): return datetime.datetime.strptime(x, "%Y-%m-%d")
-    df = pd.read_csv(filename, parse_dates=['date'], date_parser=dateparser)
+    df = pd.read_csv(filename,
+                     parse_dates=['date'], date_parser=dateparser,
+                     dtype={"fips": str})
     # df = pd.read_csv(filename, index_col='date', parse_dates=True)
     return df
 
@@ -27,17 +29,31 @@ def get_states(df):
     return df['state'].unique()
 
 
-def get_doubling_rate(df, key):
-    # Get the rate (number of days) for the number of {cases, deaths} to double
-    # by STATE. (This won't make any sense if you try to pass in the county data)
-    # This uses the one-day percentage increase in cases and adds it as a col.
-    # `key` specifies 'cases' or 'deaths'
-    state_changes = []
-    for state in get_states(df):
-        state_changes.append(col_filter(df, state=state)[key].pct_change())
-    change_series = pd.concat(state_changes)
-    df[key+'_change'] = change_series
-    df[key+'_doubling_rate'] = 1 / np.log2(1+df[key+'_change'])
+def process_data(df):
+    df = get_doubling_rate(df)
+    # Add text shortname
+    if 'county' in df.columns:
+        df['title'] = df['county'] + ' County, ' + df['state']
+    else:
+        df['title'] = df['state']
+    return df
+
+
+def get_doubling_rate(df, *keys):
+    """
+    Get the rate (number of days) for the number of {cases, deaths} to double by
+    state or county, based on unique FIPS codes. This uses the one-day
+    percentage increase in cases and adds it as a col. `keys` specify 'cases' or
+    'deaths'
+    """
+    for key in keys:
+        changes = []
+        for fips in df.fips.unique():
+            changes.append(col_filter(df, fips=fips)[key].pct_change())
+        change_series = pd.concat(changes)
+        # Add the change/doubling columns to the existing dataframe
+        df[key+'_change'] = change_series
+        df[key+'_doubling_rate'] = 1 / np.log2(1+df[key+'_change'])
     return df
 
 
